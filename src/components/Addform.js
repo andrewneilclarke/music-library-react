62c7a0efa240becd49b032af683e042923eaa4b7
andrewneilclarke <andrewneilclarke@gmail.com>
2021-11-08 16:09:00 +0100

    added responsive styles

--- src/components/Addform.js
+++ src/components/Addform.js
@@ -3,21 +3,6 @@ import CustomSelect from './CustomSelect'
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 import { CKEditor } from '@ckeditor/ckeditor5-react'
 
-// const Artists = [
-//     {
-//         value: 'the monkeys',
-//         label: '- The Monkeys'
-//     },
-//     {
-//         value: 'the bees',
-//         label: '- The Bees'
-//     },
-//     {
-//         value: 'the animals',
-//         label: '- The Animals'
-//     }
-// ]
-
 const Addform = ({ onSubmit, pageTitle, artists }) => {
 
     let initialValues = {
@@ -63,77 +48,78 @@ const Addform = ({ onSubmit, pageTitle, artists }) => {
     }
 
 
-
     return (
         <>
-            <form className="form flex flex-col px-16 pl-28 w-1/2" action="results.js" onSubmit={formik.handleSubmit}>
+            <div className="flex flex-col items-center justify-center">
                 <h1 className="text-xl">{pageTitle}</h1>
-                <div className="grid grid-rows-2 m-1 text-base">
-                    <label>Title</label>
-                    <input type="text" placeholder="Title" name="title" id="title" value={formik.values.title} onChange={formik.handleChange} />
-                    {formik.errors.title ? <div className="error">{formik.errors.title}</div> : null}
+                <form className="form flex flex-col items-between mt-3" action="results.js" onSubmit={formik.handleSubmit}>
+                    <div className="flex flex-col">
+                        <label>Title</label>
+                        <input type="text" placeholder="Title" name="title" id="title" value={formik.values.title} onChange={formik.handleChange} />
+                        {formik.errors.title ? <div className="error">{formik.errors.title}</div> : null}
 
-                    <label>Artist</label>
-                    <input type="text" placeholder="Add new" className="mb-3" name="artist" id="artist" value={formik.values.artist} onChange={formik.handleChange} />
-                    <CustomSelect
-                        onChange={value => formik.setFieldValue('artist', value.value)}
-                        value={formik.values.artist}
-                        options={artists}
-                    />
-                    {formik.errors.artist ? <div className='error'>{formik.errors.artist}</div> : null}
-                    <label>Album</label>
-                    <input type="text" placeholder="album" name="album" id="album" value={formik.values.album} onChange={formik.handleChange} />
-                    {formik.errors.album ? <div className="error">{formik.errors.album}</div> : null}
+                        <label className="mt-3">Artist</label>
+                        <input type="text" placeholder="Add new" className="mb-3" name="artist" id="artist" value={formik.values.artist} onChange={formik.handleChange} />
+                        <CustomSelect
+                            onChange={value => formik.setFieldValue('artist', value.value)}
+                            value={formik.values.artist}
+                            options={artists}
+                        />
+                        {formik.errors.artist ? <div className='error'>{formik.errors.artist}</div> : null}
+                        <label className="mt-3">Album</label>
+                        <input type="text" placeholder="album" name="album" id="album" value={formik.values.album} onChange={formik.handleChange} />
+                        {formik.errors.album ? <div className="error">{formik.errors.album}</div> : null}
 
-                    <label>Genre</label>
-                    <select name="genre" id="genre" className="border-gray-600" value={formik.values.genre} onChange={formik.handleChange}>
-                        <option value=""></option>
-                        <option value="Rock">Rock</option>
-                        <option value="Pop">Pop</option>
-                        <option value="Classical">Classical</option>
-                        <option value="Dance">Dance</option>
-                        <option value="Jazz">Jazz</option>
-                        <option value="Soul">Soul</option>
-                        <option value="Other">Other</option>
-                    </select>
+                        <label className="mt-3">Genre</label>
+                        <select name="genre" id="genre" className="mt-2" value={formik.values.genre} onChange={formik.handleChange}>
+                            <option value=""></option>
+                            <option value="Rock">Rock</option>
+                            <option value="Pop">Pop</option>
+                            <option value="Classical">Classical</option>
+                            <option value="Dance">Dance</option>
+                            <option value="Jazz">Jazz</option>
+                            <option value="Soul">Soul</option>
+                            <option value="Other">Other</option>
+                        </select>
 
-                    <label>Year</label>
-                    <select name="year" id="year" value={formik.values.year} onChange={formik.handleChange} >
-                        {years.map((year, index) => (
-                            <option key={`year${index}`}>{year}</option>
-                        ))}
-                    </select>
+                        <label className="mt-3">Year</label>
+                        <select name="year" id="year" className="my-2" value={formik.values.year} onChange={formik.handleChange} >
+                            {years.map((year, index) => (
+                                <option key={`year${index}`}>{year}</option>
+                            ))}
+                        </select>
 
-                    {formik.errors.year ? <div className="error">{formik.errors.year}</div> : null}
+                        {formik.errors.year ? <div className="error">{formik.errors.year}</div> : null}
 
-                    <label>Review</label>
+                        <label className="my-2">Review</label>
 
-                    <CKEditor
-                        editor={ClassicEditor}
-                        value={formik.values.review}
-                        name="review" id="review"
-                        onChange={(e, editor) => {
-                            const data = editor.getData()
-                            formik.values.review = data;
-                        }}
-                    />
+                        <CKEditor
+                            editor={ClassicEditor}
+                            value={formik.values.review}
+                            name="review" id="review"
+                            onChange={(e, editor) => {
+                                const data = editor.getData()
+                                formik.values.review = data;
+                            }}
+                        />
 
-                    {/* <textarea className="rounded-2xl" name="review" id="review" cols="1" rows="5" value={formik.values.review} onChange={formik.handleChange}></textarea> */}
+                        {/* <textarea className="rounded-2xl" name="review" id="review" cols="1" rows="5" value={formik.values.review} onChange={formik.handleChange}></textarea> */}
 
-                    <div>
-                        <button type="submit" className="transform ease-in duration-100 hover:scale-105">Save</button>
+                        <div>
+                            <button type="submit" className="mb-2 hover:text-indigo-800">Save</button>
+                        </div>
+                        <div className="fileurl">
+                            <label>Enter File Location</label>
+                            <input type="url" name="location" id="location" value={formik.values.location} onChange={formik.handleChange} />
+
+                            {/* <label className="mt-3">Locate File</label>
+                            <input type="file" name="file" id="file" value={formik.values.path} onChange={formik.handleChange} /> */}
+                        </div>
+
+
                     </div>
-                    <div className="fileurl">
-                        <label>File Location</label>
-                        <input type="url" name="location" id="location" value={formik.values.location} onChange={formik.handleChange} />
-
-                        <label>Locate</label>
-                        <input type="file" name="file" id="file" value={formik.values.path} onChange={formik.handleChange} />
-                    </div>
-
-
-                </div>
-            </form>
+                </form>
+            </div>
         </>
     )
 }
--- src/components/Editform.js
+++ src/components/Editform.js
@@ -4,20 +4,22 @@ import CustomSelect from './CustomSelect'
 
 const Editform = ({ tracks, artists, onSubmit, handleChange, closeEdit, pageTitle }) => {
     const { id } = useParams();
-    const track = tracks.filter((track) => track.id === id)
+    const track = tracks.filter((track) => track.id === id)[0];
 
     let initialValues = {
-        title: track[0].title,
-        artist: track[0].artist,
-        album: track[0].album,
-        genre: track[0].genre,
-        year: track[0].year,
-        location: track[0].location,
-        path: track[0].path,
-        comment: track[0].comment,
-        artwork: track[0].artwork,
-        id: id
+        title: track.title,
+        artist: track.artist,
+        album: track.album,
+        genre: track.genre,
+        year: track.year,
+        location: track.location,
+        path: track.path,
+        comment: track.comment,
+        artwork: track.artwork,
+        id
     }
+
+    // initialValues();
 
     const validate = values => {
         let errors = {}
@@ -49,16 +51,16 @@ const Editform = ({ tracks, artists, onSubmit, handleChange, closeEdit, pageTitl
     }
 
     return (
-        <>
+        <div className="flex flex-col items-center justify-center">
+            <h1 className="text-xl">{pageTitle}</h1>
 
-            <form className="form flex flex-col px-16 pl-28 max-w-lg" action="results.js" onSubmit={formik.handleSubmit} onChange={formik.handleChange}>
-                <h1 className="text-xl">{pageTitle}</h1>
-                <div className="grid grid-rows-2 m-2">
-                    <label htmlFor="songtitle">Title</label>
+            <form className="form flex flex-col items-between mt-3" action="results.js" onSubmit={formik.handleSubmit} onChange={formik.handleChange}>
+                <div className="flex flex-col">
+                    <label className="">Title</label>
                     <input type="text" placeholder="title" name="title" id="title" value={formik.values.title} onChange={formik.handleChange} />
                     {formik.errors.title ? <div className="error">{formik.errors.title}</div> : null}
 
-                    <label>Artist</label>
+                    <label className="mt-3">Artist</label>
                     <input type="text" placeholder="Add new" className="mb-3" name="artist" id="artist" value={formik.values.artist} onChange={formik.handleChange} />
                     <CustomSelect
                         onChange={value => formik.setFieldValue('artist', value.value)}
@@ -67,12 +69,12 @@ const Editform = ({ tracks, artists, onSubmit, handleChange, closeEdit, pageTitl
                     />
                     {formik.errors.artist ? <div className='error'>{formik.errors.artist}</div> : null}
 
-                    <label htmlFor="album">Album</label>
+                    <label className="mt-3">Album</label>
                     <input type="text" placeholder="album" name="album" id="album" value={formik.values.album} onChange={formik.handleChange} />
                     {formik.errors.album ? <div className="error">{formik.errors.album}</div> : null}
 
-                    <label htmlFor="genre">Genre</label>
-                    <select name="genre" id="genre" className="border-gray-600" value={formik.values.genre} onChange={formik.handleChange}>
+                    <label className="mt-3">Genre</label>
+                    <select name="genre" id="genre" className="mt-2" value={formik.values.genre} onChange={formik.handleChange}>
                         <option value="Rock">Rock</option>
                         <option value="Pop">Pop</option>
                         <option value="Classical">Classical</option>
@@ -82,21 +84,21 @@ const Editform = ({ tracks, artists, onSubmit, handleChange, closeEdit, pageTitl
                         <option value="Other">Other</option>
                     </select>
 
-                    <label htmlFor="year">Year</label>
-                    <select name="year" id="year" value={formik.values.year} onChange={formik.handleChange} >
+                    <label className="mt-3">Year</label>
+                    <select name="year" id="year" className="mt-2" value={formik.values.year} onChange={formik.handleChange} >
                         {years.map((year, index) => (
                             <option key={`year${index}`}>{year}</option>
                         ))}
                     </select>
 
                     {formik.errors.year ? <div className="error">{formik.errors.year}</div> : null}
-                    <div>
+                    <div className="flex justify-end">
                         {/* <button onClick={() => setFormValues(initialValues)} type="button" className="transform ease-in duration-100 hover:scale-105">Load</button> */}
-                        <button type="submit" className="transform ease-in duration-100 hover:scale-105">Save</button>
-                        <button onClick={closeEdit} type="button">Close</button>
+                        <button type="submit" className="mr-3 hover:text-indigo-800">Save</button>
+                        <button onClick={closeEdit} type="button" className="hover:text-indigo-800">Close</button>
                     </div>
                     <div className="fileurl">
-                        <label htmlFor="location">File Location</label>
+                        <label className="mt-3">File Location</label>
                         <input type="url" name="location" id="location" value={formik.values.location} onChange={formik.handleChange} />
 
                         <label htmlFor="file">Locate</label>
@@ -109,7 +111,7 @@ const Editform = ({ tracks, artists, onSubmit, handleChange, closeEdit, pageTitl
                 </div>
             </form>
 
-        </>
+        </div>
 
     )
 }
